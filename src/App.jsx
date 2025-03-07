f5ec2fc4ee8d7877766615cd00deba187710547e
Kuldeep Kumar <upsainian3330@gmail.com>
2025-03-07 19:54:18 +0530

    temperature controller

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import RandomizeColor from '../02-Random-color/RandomizeColor'
 import LikePhoto from '../03-Like-my-photo/LikePhoto'
 import Testimonial from '../04-Testimonials/Testimonial'
 import AlertApp from '../05-Alerts/AlertApp'
+import TemperatureController from '../06-Temperature-Controller/TemperatureController'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <AlertApp/>
+    <TemperatureController/>
+    {/* <AlertApp/> */}
     {/* <Testimonial/> */}
     {/* <LikePhoto/> */}
     {/* <RandomizeColor/> */}
